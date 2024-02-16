8a034f0eff7e10d5e12285a475981c616540c684
KirigayaWin <158750460+KirigayaWin@users.noreply.github.com>
2024-02-17 07:13:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let week2End = new Date('2024-02-09');
     let week3Start = new Date('2024-02-10');
     let week3End = new Date('2024-02-16');
-    let week4Start = new Date('2024-02-16');
+    let week4Start = new Date('2024-02-17');
     let week4End = new Date('2024-02-23');
     let week5Start = new Date('2024-02-24');
     let week5End = new Date('2024-03-01');
